08b6177b5e2fb963b612e5e6090bc39e4ca68431
Kasper Rosenkrands <krosen17@student.aau.dk>
2021-12-29 22:36:36 +0100

    Added function generate_2d_instance

---
+++ R/generate_2d_instance.R
@@ -0,0 +1,35 @@
+#' Generate a problem instance
+#'
+#' @param seed Seed to ensure reproducibility
+#' @param no_of_points The number of demand points
+#' @param interval A named vector of the range for the x and y axis
+#' @param arv A named vector of the range in the arrival rate
+#'
+#' @return A list
+#' @export
+#'
+#' @examples
+#' generate_2d_instance()
+generate_2d_instance <- function(
+  seed = NULL,
+  no_of_points = 50,
+  interval = c("min" = -10, "max" = 10),
+  arv = c("min" = 0, "max" = 80)
+) {
+  id <- 1:no_of_points
+  set.seed(seed)
+  x <- stats::runif(no_of_points, min = interval["min"], max = interval["max"])
+  y <- stats::runif(no_of_points, min = interval["min"], max = interval["max"])
+  set.seed(NULL)
+  arrival_rate <- 1/round(stats::runif(no_of_points, min = arv["min"]*60, max = arv["max"]*60))
+  data <- tibble::tibble(
+    "Demand point id" = as.character(id),
+    "x" = x,
+    "y" = y,
+    "Arrival rate" = arrival_rate
+  ) %>%
+    dplyr::mutate(prob = cumsum(`Arrival rate`/sum(`Arrival rate`)))
+  results <- list("data" = data, "interval" = interval,
+                  "no_of_points" = no_of_points, "arv" = arv)
+  return(results)
+}
