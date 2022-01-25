f460f6bc74975d2f669d1d7fbd720fd5a70ba6dc
Kasper Rosenkrands <krosen17@student.aau.dk>
2022-01-25 13:25:26 +0100

    Added SWKM algorithm for solve_wkmeans

--- R/solve_wkmeans.R
+++ R/solve_wkmeans.R
@@ -2,6 +2,7 @@
 #'
 #' @param instance A list returned from generate_2d_instance
 #' @param no_of_centers The number of base locations to generate
+#' @param type The type of WKmeans implematation to use, options are "flexclust" or "swkm"
 #'
 #' @return A list
 #' @export
@@ -9,16 +10,26 @@
 #' @examples
 #' instance <- generate_2d_instance()
 #' # solution <- solve_wkmeans(instance = instance, no_of_centers = 5)
-solve_wkmeans <- function(instance, no_of_centers = 5) {
-  coordinates <- instance$data %>% dplyr::select(.data$x, .data$y)
+solve_wkmeans <- function(instance, no_of_centers = 5, type = c("flexclust", "swkm")) {
+  if (type == "flexclust") {
+    coordinates <- instance$data %>% dplyr::select(.data$x, .data$y)
+    centroids <- flexclust::stepFlexclust(x = coordinates, k = no_of_centers, nrep = 10,
+                                          weights = instance$data$`Arrival rate`, FUN = "cclust",
+                                          dist = "euclidean", verbose = F,
+                                          method = "hardcl", control = list(iter.max = 1000))
+    #
+    clusters <- as.data.frame(centroids@centers)
+    clustering_vector <- centroids@cluster
+  } else if (type == "swkm") {
+    centroids <- SWKM::kmeans.weight(
+      x = instance$data %>% dplyr::select(x,y) %>% data.matrix(),
+      K = no_of_centers,
+      weight = instance$data$`Arrival rate`
+    )
 
-  centroids <- flexclust::stepFlexclust(x = coordinates, k = no_of_centers, nrep = 100000,
-                                        weights = instance$data$`Arrival rate`, FUN = "cclust",
-                                        dist = "euclidean", verbose = F,
-                                        method = "hardcl", control = list(iter.max = 100))
-  #
-  clusters <- as.data.frame(centroids@centers)
-  clustering_vector <- centroids@cluster
+    clusters <- as.data.frame(centroids$centers) %>% dplyr::rename(x = V1, y = V2)
+    clustering_vector <- centroids$cluster
+  }
 
   instance <- instance$data %>%
     dplyr::mutate("Centroid id" = clustering_vector %>% as.character()) %>%
