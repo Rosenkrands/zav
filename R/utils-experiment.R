4bb7260796ca93297e88c993aba297aea301d530
Kasper Rosenkrands <krosen17@student.aau.dk>
2022-01-26 10:45:21 +0100

    added two objective functions and worked on generate_solutions

--- R/utils-experiment.R
+++ R/utils-experiment.R
@@ -75,3 +75,64 @@ generate_instances <- function(n = 5,
 
   saveRDS(object = metadata, file = "instance_metadata.rds")
 }
+
+generate_solution <- function(methods = c("km", "wkm"), no_of_centers = c(5, 15)) {
+  # first we make a instance directory if not already present
+  if (dir.exists("solutions")) {
+    message("A solution directory was found so new instances will be placed in the existing directory")
+  } else {
+    message("No solution directory was found so a new one will be created")
+    dir.create("solutions")
+  }
+
+  # find instances and load into a list
+  instances <- lapply(
+    list.files("instances") %>% as.list(),
+    function(instance_file) readRDS(paste0("./instances/", instance_file))
+  )
+  names(instances) <- tools::file_path_sans_ext(list.files("instances"))
+
+  # generate solution parameters based on instances and methods
+  params <- expand.grid(names(instances), methods, no_of_centers) %>%
+    dplyr::rename(instance = Var1, method = Var2, no_of_centers = Var3)
+
+  params_list <- split(params, 1:nrow(params))
+
+  solve_and_save <- function(param) {
+    # filename for the solution
+    file = paste0(
+      "./solutions/",
+      hexadec(size = 4),'_',
+      param$instance,'_',
+      param$method,'_',
+      param$no_of_centers,'.rds'
+    )
+
+    # choose solution method based on param$method
+    if (param$method == "km") {
+      solution <- solve_kmeans(instance = instances[[param$instance]],
+                               no_of_centers = param$no_of_centers)
+    } else if (param$method == "wkm") {
+      solution <- solve_wkmeans(instance = instances[[param$instance]],
+                                no_of_centers = param$no_of_centers)
+    } else {
+      stop(paste0("method '", param$method, "' not implemented."))
+    }
+
+    # save solution
+    saveRDS(solution, file = file)
+  }
+
+  # generating solution in parallel
+  num_cores <- parallel::detectCores(logical = F)
+  cl <- parallel::makeCluster(num_cores)
+
+  invisible(parallel::clusterExport(cl, c('instances')))
+  invisible(parallel::clusterEvalQ(cl, library(zav)))
+  pbapply::pblapply(params_list, solve_and_save, cl = cl) -> res
+
+  parallel::stopCluster(cl)
+
+  # generating solution metadata
+
+}
